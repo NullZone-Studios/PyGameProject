c2e3fde29e111a565d14c18b7b742f6d1cf1eb9d
PentaTrax <philipnordnielsen@gmail.com>
2026-02-14 23:49:09 +0100

    small update to fix font

--- Builder/ShowcaseScripts/positionToLabel.py
+++ Builder/ShowcaseScripts/positionToLabel.py
@@ -8,5 +8,5 @@ class PositionToLabel(Script):
         
     def Update(self, deltaTime):
         position = self.GameObject.Transform.Position
-        self.label.Text = f"X= {round(position.x, 2)} \nY= {round(position.y,2)} \nZ= {round(position.z)}"
+        self.label.Text = f"X: {round(position.x, 2)} \nY: {round(position.y,2)} \nZ: {round(position.z)}"
         return super().Update(deltaTime)
--- Builder/showcase.py
+++ Builder/showcase.py
@@ -140,7 +140,7 @@ class Showcase(GameBuilder):
         canvas: UI.Canvas = uiObject.AddComponent(UI.Canvas(self.RESOLUTION.x, self.RESOLUTION.y, inputSystem=engine.input))
         positionLabel = canvas.root.AddChild(UI.Label("positionLabel", "position"))
         positionLabel.style = UI.Style(
-            font=pygame.font.SysFont("Aria", 32),
+            font=pygame.font.SysFont("arial", 32),
             padding=5
         )
         gameObjects.append(uiObject)
